fe60ff99767d567e63ecb20ae61370c7f5ec8c23
AytajHsnv <aytajhasanova99@gmail.com>
2024-10-22 16:16:31 +0000

    DeepLabV3+ ResNet50_crack500 gamma based results

--- evaluate_crack.py
+++ evaluate_crack.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, default='./results.prf')
 parser.add_argument('--thresh_step', type=float, default=0.01)
 args = parser.parse_args()
-model = 'DeepLabV3+ ResNet50_DeepCrack based(gamma)'
+model = 'DeepLabV3+ ResNet50_Crack500 based(gamma)'
 
 def cal_prf_metrics(pred_list, gt_list, distance=[], angle=[], thresh_step=0.01, img_names=None):
     final_accuracy_all = []
