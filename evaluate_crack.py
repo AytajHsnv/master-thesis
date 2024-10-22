a81f5959a051203b0ba3c424734e77b8bb532db7
AytajHsnv <aytajhasanova99@gmail.com>
2024-10-22 15:50:54 +0000

    DeepLabV3+ MobileNet_Deepcrack gamma function based results

--- evaluate_crack.py
+++ evaluate_crack.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, default='./results.prf')
 parser.add_argument('--thresh_step', type=float, default=0.01)
 args = parser.parse_args()
-model = 'DeepLabV3+ MobileNet_Crack500 based'
+model = 'DeepLabV3+ MobileNet_DeepCrack based'
 
 def cal_prf_metrics(pred_list, gt_list, distance=[], angle=[], thresh_step=0.01, img_names=None):
     final_accuracy_all = []
