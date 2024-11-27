8975422e1bcbd48d5c11e83b1e662acf53eb5c7f
AytajHsnv <aytajhasanova99@gmail.com>
2024-11-27 12:53:28 +0000

    MobileNet_combined results

--- evaluate_crack.py
+++ evaluate_crack.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, default='./results.prf')
 parser.add_argument('--thresh_step', type=float, default=0.01)
 args = parser.parse_args()
-model = 'DeepLabV3+_MobileNet_Crack500 Labphotos'
+model = 'DeepLabV3+_MobileNet_Combined Labphotos'
 folder = ['gain', 'gamma', '']
 def cal_prf_metrics(pred_list, gt_list, distance=[], angle=[], thresh_step=0.01, img_names=None):
     final_accuracy_all = []
