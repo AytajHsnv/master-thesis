575b2e77dd7fb18279fc4e7bb26f16922ba63321
AytajHsnv <aytajhasanova99@gmail.com>
2024-11-29 16:23:53 +0000

    MobileNet_Crack500 gamma results

--- evaluate_crack.py
+++ evaluate_crack.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, default='./results.prf')
 parser.add_argument('--thresh_step', type=float, default=0.01)
 args = parser.parse_args()
-model = 'DeepLabV3+_MobileNet_DeepCrack Labphotos_Gamma'
+model = 'DeepLabV3+_MobileNet_Crack500 Labphotos_Gamma'
 folder = ['gain', 'gamma', '']
 def cal_prf_metrics(pred_list, gt_list, distance=[], angle=[], thresh_step=0.01, img_names=None):
     final_accuracy_all = []
